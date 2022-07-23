89c696fe9591408759e9311e14bafe8f70f6ee37
Scott Standaert <scott.standaert@gmail.com>
2022-07-23 00:10:00 -0700

    finish moving code to js file.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,19 +1,30 @@
 let computerScore = 0
 let playerScore = 0
 
+//starts game for testing
+let playerSelection = 'Rock';
+let computerSelection = 'Paper';
+console.log(playRound(playerSelection, computerSelection))
+
 //function that plays 1 round of Rock Paper Scissors. Takes 2 parameters, playerSelection and computerSelection and then returns string declaring winner
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
+        computerScore++
         return 'You lose! Paper beats rock!';
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
+        computerScore++
         return 'You lose! Scissors beats paper!';
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
+        computerScore++
         return 'You lose! Rock beats paper!';
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') {
+        playerScore++
         return 'You win! Paper beats rock!';
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') {
+        playerScore++
         return 'You win! Scissors beat paper!';
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') {
+        playerScore++
         return 'You win! Rock beats scissors!';
     } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return 'You\'ve tied!';
@@ -34,3 +45,10 @@ function decideWinner(playerScore, computerScore) {
 }
 
 
+//function called getComputerChoice that will random return either 'Rock','Paper' or 'Scissors'
+        
+        //const compArray = ['Rock', 'Paper', 'Scissors'];
+         //const randomChoice = compArray[Math.floor(Math.random() * compArray.length)];
+        //console.log(randomChoice);
+
+//need to finish getComputerChoice() function, playerChoice prompt and game() function
